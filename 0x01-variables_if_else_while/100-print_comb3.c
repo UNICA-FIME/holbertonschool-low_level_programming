7eb55d49d6e000872d52e64891a556efadfc363b
juan20carlos <juancarlos14089@gmail.com>
2022-02-05 15:45:48 +0000

    Version 1 100-print_comb3.c file

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * @void: any
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	unsigned int n;
+	unsigned int m;
+
+	n = 48;
+	while (n <= 57)
+	{
+		m = 48;
+		while (m <= 57)
+		{
+			if (n < m && n != m)
+			{
+				putchar(n);
+				putchar(m);
+				putchar(32);
+			}
+			m++;
+		}
+		n++;
+	}
+	putchar(10);
+	return (0);
+}
