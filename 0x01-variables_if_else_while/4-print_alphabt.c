2d13554ca0b53f7df5bbf1e22205f14b0055a587
juan20carlos <juancarlos14089@gmail.com>
2022-02-05 00:07:37 +0000

    version 1 4-print_alphabt.c file

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * @void: any
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	unsigned int n;
+
+	n = 97;
+	while (n <= 122)
+	{
+		if (n != 101 && n != 113)
+		{
+			putchar(n);
+		}
+		n++;
+	}
+	putchar(10);
+	return (0);
+}
