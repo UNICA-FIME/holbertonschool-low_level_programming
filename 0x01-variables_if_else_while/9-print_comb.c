b9d137d9d1c061d8cd6a464fb3d7d2c981e31362
juan20carlos <juancarlos14089@gmail.com>
2022-02-05 01:56:50 +0000

    Version 1 9-print_comb.c file

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * @void: any
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	unsigned int n;
+
+	n = 48;
+	while (n <= 57)
+	{
+		putchar(n);
+		if (n != 57)
+		{
+			putchar(44);
+			putchar(32);
+		}
+		n++;
+	}
+	putchar(10);
+	return (0);
+}
