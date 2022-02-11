2f998c5d5a4afa544b2795e8fcbe2f447496312c
juan20carlos <juancarlos14089@gmail.com>
2022-02-11 00:19:30 +0000

    Version 1 5-more_numbers.c file

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,37 @@
+#include "main.h"
+/**
+ * more_numbers -  prints 10 times the numbers, from 0 to 14
+ *
+ * @void: any
+ */
+void more_numbers(void)
+{
+	unsigned int m;
+	unsigned int p;
+	unsigned int r;
+
+	r = 0;
+	while (r <= 14)
+	{
+		m = 48;
+		while (m <= 49)
+		{
+			p = 48;
+			while (p <= 57)
+			{
+				if (m == 49 && p < 53)
+				{
+					_putchar(m);
+				}
+				if (m == 48 || p <= 52)
+				{
+					_putchar(p);
+				}
+				p++;
+			}
+			m++;
+		}
+		_putchar(10);
+		r++;
+	}
+}
