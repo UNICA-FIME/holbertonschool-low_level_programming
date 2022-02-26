e2e47c476b1f083cf80174a37ec8f97dc7630b55
juan20carlos <juancarlos14089@gmail.com>
2022-02-26 00:14:09 +0000

    Version 1 0-whatsmyname.c file

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Entry point with two argument
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0
+ *
+ */
+int main(int  __attribute__((unused)) argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
