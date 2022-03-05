e2727a3c12d4c424c1aa200d445f2e4ee75c8e29
juan20carlos <juancarlos14089@gmail.com>
2022-03-05 00:08:47 +0000

    Version 1 4-free_grid.c file

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -10,10 +10,15 @@ void free_grid(int **grid, int height)
 {
 	int i;
 
+	if (grid == NULL || height <= 0)
+	{
+		return;
+	}
 	i = 0;
 	while (i < height)
 	{
 		free(grid[i]);
+		i++;
 	}
 	free(grid);
 }
