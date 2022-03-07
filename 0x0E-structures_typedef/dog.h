26a7ef5e81d11c53e8f14387b7b12883bde41cbe
juan20carlos <juancarlos14089@gmail.com>
2022-03-07 22:05:20 +0000

    Version 1 dog.h file

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef _STRUCT_
+#define _STRUCT_
+/**
+ * struct dog - create a new struct
+ * @name: element name
+ * @age: element age
+ * @owner: element owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
