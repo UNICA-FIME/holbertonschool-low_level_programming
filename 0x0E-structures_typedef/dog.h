032c020256afe2debd1e9438930f1bf32823dc65
juan20carlos <juancarlos14089@gmail.com>
2022-03-08 17:28:50 +0000

    Header

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -5,13 +5,14 @@
  * @name: element name
  * @age: element age
  * @owner: element owner
+ * dog_t:  new type of struct
  */
 typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-}dog_t;
+} dog_t;
 void print_dog(struct dog *d);
 void init_dog(struct dog *d, char *name, float age, char *owner);
 #endif
