ed1fc5517929b9839a9a83fb90bce006bc50932f
juan20carlos <juancarlos14089@gmail.com>
2022-03-11 02:35:01 +0000

    Version 0-sum_them_all.c file

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -8,13 +8,12 @@ int sum_them_all(const unsigned int n, ...)
 {
 	unsigned int i;
 	int sum;
+	va_list ap;
 
 	if (n == 0)
 	{
 		return (0);
 	}
-	va_list ap;
-
 	va_start(ap, n);
 	i = 0;
 	sum = 0;
