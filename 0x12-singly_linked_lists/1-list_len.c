9125a48636f059f7328fc2046cb339212a811ca0
juan20carlos <juancarlos14089@gmail.com>
2022-03-22 01:59:12 +0000

    Version first file

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+ * list_len - finds the number of elements in a linked list
+ * @h: pointer to the start of the linked list
+ *
+ * Return: number of elements in the linked list
+ */
+size_t list_len(const list_t *h)
+{
+	size_t count;
+	const list_t *actual;
+
+	count = 0;
+	actual = h;
+	while (actual != NULL)
+	{
+		actual = actual->next;
+		count++;
+	}
+	return (count);
+}
