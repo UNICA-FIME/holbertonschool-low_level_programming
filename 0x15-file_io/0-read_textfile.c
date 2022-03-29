6995b51d22c9c51e7869089986767a9e25b2431d
juan20carlos <juancarlos14089@gmail.com>
2022-03-29 20:49:47 +0000

    Version 1.1

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -25,7 +25,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (buff == NULL)
 	{
 		free(buff);
-		return (NULL);
+		return (0);
 	}
 	lenr = read(fd, buff, letters);
 	close(fd);
