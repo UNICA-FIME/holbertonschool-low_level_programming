3a439b148c0cb1b10c64ad5d90ad4124797ea7ae
UNICA-FIME <juancarlos14089@gmail.com>
2022-05-30 06:16:53 +0000

    Version

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,12 +9,19 @@
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *array = NULL;
+	hash_table_t *h_array = NULL;
 
-	array = (hash_table_t *)malloc(sizeof(hash_table_t) * size);
-	if (array == NULL)
+	h_array = (hash_table_t *)malloc(sizeof(hash_table_t));
+	if (h_array == NULL)
 	{
 		return (NULL);
 	}
-	return (array);
+	h_array->size = size;
+	h_array->array = malloc(sizeof(hash_node_t *) * size);
+	if (h_array->array == NULL)
+	{
+		free(h_array);
+		return (NULL);
+	}
+	return (h_array);
 }
