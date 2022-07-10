a51d4db59dafbc68b292550544259dfdde7028ab
UNICA-FIME <juancarlos14089@gmail.com>
2022-07-10 03:46:57 +0000

    Header

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,5 +1,7 @@
 #ifndef _VARADIC_
 #define _VARADIC_
 #include<stdarg.h>
+#include<stdio.h>
 int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
 #endif
