b122a4211d8375c0b6cdf259399bcb8776defc92
UNICA-FIME <juancarlos14089@gmail.com>
2022-07-10 04:40:10 +0000

    version 1

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -26,7 +26,10 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		}
 		if (i < n - 1)
 		{
-			printf("%s", separator);
+			if (separator != NULL)
+			{
+				printf("%s", separator);
+			}
 		}
 	}
 	va_end(ap);
