8e309cd797808680194bcca3a0bd20026733772e
UNICA-FIME <juancarlosfime@gmail.com>
2022-08-31 02:52:06 +0000

    Header

--- search_algorithms/search_algos.h
+++ search_algorithms/search_algos.h
@@ -4,4 +4,6 @@
 #include <stdio.h>
 
 int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+void printf_array(int *array, size_t size, size_t size_2);
 #endif
