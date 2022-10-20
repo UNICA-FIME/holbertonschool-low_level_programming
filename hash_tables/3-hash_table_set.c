3d448038b2b00b523e8b9c39d238a2339d2f0d65
UNICA-FIME <juancarlosfime@gmail.com>
2022-10-20 00:40:19 +0000

    error correction

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -1,81 +1,60 @@
 #include "hash_tables.h"
-/**
- * new_nodo - creates a new hash node
- * @key: key for the node
- * @value: for the node
- *
- * Return: the new node, or NULL on failure
- */
 
-hash_node_t *new_nodo(const char *key, const char *value)
-{
-	hash_node_t *new = NULL;
 
-	new = malloc(sizeof(hash_node_t));
-	if (new == NULL)
-	{
-		free(new);
-		return (0);
-	}
-	new->key = strdup(key);
-	if (new->key == NULL)
-	{
-		free(new);
-		return (NULL);
-	}
-	new->value = strdup(value);
-	if (new->value == NULL)
-	{
-		free(new->value);
-		free(new);
-		return (NULL);
-	}
-	new->next = NULL;
-	return (new);
-}
 /**
- * hash_table_set - sets a key to a value in the hash table
- * @ht: hash table to add elemt to
- * @key: key for the data
- * @value: data to store
- *
+ * hash_table_set - adds an element to a hash table
+ * @ht: address of a given hash table
+ * @key: key of the element
+ * @value: value of the element
  * Return: 1 if successful, 0 otherwise
- *
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index;
-	hash_node_t *new_1 = NULL;
-	hash_node_t *tmp;
-	char *new_value;
+	hash_node_t *new;
 
-	if (*key == '\0' || *value == '\0' || ht->size == 0
-	   || ht == NULL || ht->array == NULL || strlen(key) == 0)
-	{
+	if (!key || !(*key) || !value || !ht)
 		return (0);
-	}
-	index = key_index((unsigned char *)key, ht->size);
-	while (tmp != NULL)
+	index = key_index((const unsigned char *)key, ht->size);
+	if (hash_table_check(ht, key, index))
 	{
-		if (strcmp(tmp->key, key) == 0)
-		{
-			new_value = strdup(value);
-			if (new_value == NULL)
-			{
-				return (0);
-			}
-			free(tmp->value);
-			tmp->value = new_value;
-			return (1);
-		}
-		tmp = tmp->next;
+		free(ht->array[index]->value);
+		ht->array[index]->value = strdup(value);
+		if (!ht->array[index]->value)
+			return (0);
+		return (1);
 	}
-	new_1 = new_nodo(key, value);
-	if (new_1 == NULL)
-	{
+	new = malloc(sizeof(hash_node_t));
+	if (!new)
 		return (0);
-	}
-	new_1->next = ht->array[index];
-	ht->array[index] = new_1;
+	new->key = strdup(key);
+	if (!new->key)
+		return (0);
+	new->value = strdup(value);
+	if (!new->value)
+		return (0);
+	new->next = ht->array[index];
+	ht->array[index] = new;
+	return (1);
+}
+
+/**
+ * hash_table_check - checks if a key already exists in ht
+ * @ht: address of a given hash table
+ * @key: key of the element
+ * @index: index of key
+ * Return: 1 if it exists, 0 otherwise
+ */
+size_t
+hash_table_check(const hash_table_t *ht, const char *key, unsigned int index)
+{
+	hash_node_t *node;
+
+	node = ht->array[index];
+
+	for (; node && strcmp(node->key, key); node = node->next)
+		;
+	if (!node)
+		return (0);
 	return (1);
 }
